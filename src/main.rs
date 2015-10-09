0998e270a269a0114f86d265c6cbab1c6b64c4af
Livio Ribeiro <livioribeiro@outlook.com>
2015-10-08 21:03:21 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate clap;
+extern crate regex;
+
+use clap::{Arg, App, AppSettings, SubCommand};
+
+fn main() {
+    let matches = App::new("cargo-readme")
+        .version(&*format!("v{}", crate_version!()))
+        // We have to lie about our binary name since this will be a third party
+        // subcommand for cargo but we want usage strings to generated properly
+        .bin_name("cargo")
+        // Global version uses the version we supplied (Cargo.toml) for all subcommands as well
+        .settings(&[AppSettings::GlobalVersion,
+                    AppSettings::SubcommandRequired])
+        // We use a subcommand because everything parsed after `cargo` is sent to the third party
+        // plugin which will then be interpreted as a subcommand/positional arg by clap
+        .subcommand(SubCommand::with_name("readme")
+            .author("Livio Ribeiro <livioribeiro@outlook.com>")
+            .about("Generate README.md from docstrings"))
+        .get_matches();
+}
