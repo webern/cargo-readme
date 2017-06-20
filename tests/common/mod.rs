a64e5f67587db90b8814010bb092c9b07a3df310
Livio Ribeiro <livioribeiro@outlook.com>
2017-06-20 20:15:28 -0300

    improve tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -6,10 +6,14 @@ use std::path::PathBuf;
 use std::process::{Command, Output};
 
 pub fn cargo_readme(args: &[&str]) -> (String, String, i32) {
+    chdir_cargo_readme("test-project", args)
+}
+
+pub fn chdir_cargo_readme(chdir: &str, args: &[&str]) -> (String, String, i32) {
     // I hope you're running "cargo test" from the project root
     let root = env::current_dir().expect("no current dir");
 
-    let test_project = root.join("tests/test-project");
+    let test_project = root.join("tests").join(chdir);
     let cargo_readme = root.join("target/debug/cargo-readme");
 
     let mut command = Command::new(cargo_readme);
@@ -22,48 +26,7 @@ pub fn cargo_readme(args: &[&str]) -> (String, String, i32) {
     parse_result(result)
 }
 
-pub fn custom_manifest_cargo_readme(manifest: &str, args: &[&str]) -> (String, String, i32) {
-    let path = create_cargo_toml(manifest);
-
-    // I hope you're running "cargo test" from the project root
-    let root = env::current_dir().expect("no current dir");
-
-    let test_project = path.parent().unwrap();
-    let cargo_readme = root.join("target/debug/cargo-readme");
-
-    let mut command = Command::new(cargo_readme);
-    command.current_dir(test_project);
-    command.arg("readme");
-    command.args(args);
-
-    let result = command.output().expect("error executing cargo readme");
-
-    parse_result(result)
-}
-
-fn create_cargo_toml(manifest: &str) -> PathBuf {
-    let now = SystemTime::now();
-    let t = now.duration_since(time::UNIX_EPOCH).expect(
-        "SystemTime failed",
-    );
-    let secs = t.as_secs();
-    let nanos = t.subsec_nanos();
-
-    let mut dir = env::temp_dir();
-    dir.push(format!("cargo-readme/test/{}{}", secs, nanos));
-    fs::create_dir_all(&dir).expect("cannot create temporary project dir");
-
-    let mut path = dir;
-    path.push("Cargo.toml");
-    let mut cargo_toml = File::create(&path).expect("cannot create Cargo.toml");
-    cargo_toml.write_all(manifest.as_bytes()).expect(
-        "cannot write to Cargo.toml",
-    );
-
-    path
-}
-
-pub fn parse_result(result: Output) -> (String, String, i32) {
+fn parse_result(result: Output) -> (String, String, i32) {
     let stdout = String::from_utf8(result.stdout)
         .expect("invalid utf-8 stdout")
         .trim()
--- tests/no-entrypoint-fail.rs
+++ tests/no-entrypoint-fail.rs
@@ -2,15 +2,9 @@
 
 mod common;
 
-const CARGO_TOML: &str = r#"
-[package]
-name = "no-entrypoint"
-version = "0.1.0"
-"#;
-
 #[test]
 fn test() {
-    let (_stdout, stderr, status) = common::custom_manifest_cargo_readme(CARGO_TOML, &[]);
+    let (_stdout, stderr, status) = common::chdir_cargo_readme("no-entrypoint-fail", &[]);
     assert_eq!("Error: No entrypoint found", stderr.trim());
     assert_eq!(status, 1);
 }
