573b971fcebdfa9e04de9124dc57b285ccf6fa87
Livio Ribeiro <livio@codata.pb.gov.br>
2017-08-23 08:36:27 -0300

    fix code blocks not being properly detected

--- src/readme/transform.rs
+++ src/readme/transform.rs
@@ -8,6 +8,10 @@ use std::iter::{Iterator, IntoIterator};
 
 use regex::Regex;
 
+const REGEX_CODE_RUST: &'static str = r"^```(rust|((rust,)?(no_run|ignore|should_panic)))?$";
+const REGEX_CODE_TEXT: &'static str = r"^```text$";
+const REGEX_CODE_OTHER: &'static str = r"^```\w[\w,\+]*$";
+
 pub trait DocTransform {
     fn transform_doc(self, indent_headings: bool) -> DocTransformer<Self>
     where
@@ -31,6 +35,7 @@ pub struct DocTransformer<I: Iterator> {
     indent_headings: bool,
     section: Code,
     re_code_rust: Regex,
+    re_code_text: Regex,
     re_code_other: Regex,
 }
 
@@ -40,15 +45,18 @@ impl<I: Iterator<Item = String>> DocTransformer<I> {
         indent_headings: bool,
     ) -> Self {
         // Is this code block rust?
-        let re_code_rust = Regex::new(r"^```(no_run|ignore|should_panic)?$").unwrap();
+        let re_code_rust = Regex::new(REGEX_CODE_RUST).unwrap();
+        // Is this code block just text?
+        let re_code_text = Regex::new(REGEX_CODE_TEXT).unwrap();
         // Is this code block a language other than rust?
-        let re_code_other = Regex::new(r"^```\w*$").unwrap();
+        let re_code_other = Regex::new(REGEX_CODE_OTHER).unwrap();
 
         DocTransformer {
             iter: iter.into_iter(),
             indent_headings: indent_headings,
             section: Code::None,
             re_code_rust: re_code_rust,
+            re_code_text: re_code_text,
             re_code_other: re_code_other,
         }
     }
@@ -80,6 +88,9 @@ where
         } else if self.section == Code::None && self.re_code_rust.is_match(&line) {
             self.section = Code::Rust;
             line = "```rust".to_owned();
+        } else if self.section == Code::None && self.re_code_text.is_match(&line) {
+            self.section = Code::Other;
+            line = "```".to_owned();
         } else if self.section == Code::None && self.re_code_other.is_match(&line) {
             self.section = Code::Other;
         } else if self.section != Code::None && line == "```" {
@@ -201,6 +212,80 @@ mod tests {
     fn transform_rust_code_block() {
         let input: Vec<_> = INPUT_RUST_CODE_BLOCK.lines().map(|x| x.to_owned()).collect();
         let expected: Vec<_> = EXPECTED_RUST_CODE_BLOCK.lines().map(|x| x.to_owned()).collect();
+
+        let result: Vec<_> = DocTransformer::new(input, true).collect();
+
+        assert_eq!(result, expected);
+    }
+
+    const INPUT_RUST_CODE_BLOCK_RUST_PREFIX: &'static str = concat_lines!(
+        "```rust",
+        "let block = \"simple code block\";",
+        "```",
+        "",
+        "```rust,no_run",
+        "let run = false;",
+        "```",
+        "",
+        "```rust,ignore",
+        "let ignore = true;",
+        "```",
+        "",
+        "```rust,should_panic",
+        "panic!(\"at the disco\");",
+        "```",
+        "",
+        "```C",
+        "int i = 0; // no rust code",
+        "```",
+    );
+
+    #[test]
+    fn transform_rust_code_block_with_prefix() {
+        let input: Vec<_> = INPUT_RUST_CODE_BLOCK_RUST_PREFIX.lines().map(|x| x.to_owned()).collect();
+        let expected: Vec<_> = EXPECTED_RUST_CODE_BLOCK.lines().map(|x| x.to_owned()).collect();
+
+        let result: Vec<_> = DocTransformer::new(input, true).collect();
+
+        assert_eq!(result, expected);
+    }
+
+    const INPUT_TEXT_BLOCK: &'static str = concat_lines!(
+        "```text",
+        "this is text",
+        "```",
+    );
+
+    const EXPECTED_TEXT_BLOCK: &str = concat_lines!(
+        "```",
+        "this is text",
+        "```",
+    );
+
+    #[test]
+    fn transform_text_block() {
+        let input: Vec<_> = INPUT_TEXT_BLOCK.lines().map(|x| x.to_owned()).collect();
+        let expected: Vec<_> = EXPECTED_TEXT_BLOCK.lines().map(|x| x.to_owned()).collect();
+
+        let result: Vec<_> = DocTransformer::new(input, true).collect();
+
+        assert_eq!(result, expected);
+    }
+
+    const INPUT_OTHER_CODE_BLOCK_WITH_SYMBOLS: &'static str = concat_lines!(
+        "```html,django",
+        "{% if True %}True{% endif %}",
+        "```",
+        "",
+        "```html+django",
+        "{% if True %}True{% endif %}",
+        "```",
+    );
+
+    #[test]
+    fn transform_other_code_block_with_symbols() {
+        let input: Vec<_> = INPUT_OTHER_CODE_BLOCK_WITH_SYMBOLS.lines().map(|x| x.to_owned()).collect();
+        let expected: Vec<_> = INPUT_OTHER_CODE_BLOCK_WITH_SYMBOLS.lines().map(|x| x.to_owned()).collect();
 
         let result: Vec<_> = DocTransformer::new(input, true).collect();
 
