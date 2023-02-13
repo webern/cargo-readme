501767948978780035fafba8b04b0e9dbef0e767
Oliver Bøving <oliver@bvng.dk>
2023-02-13 10:04:59 +0100

    Use `cargo_metadata` to get manifest data
    This commit uses the `cargo_metadata` crate to derive information about
    the crate, as opposed to only parsing `Cargo.toml`.
    
    In particular, this helps when fields in the manifest are inherited from
    the workspace, for example if `version = { workspace = true }`.
    
    One thing `cargo_metadata` does not provide, is badges, so these are
    still parsed from `Cargo.toml`.
    
    The error messages also changed as a result of propagating the errors
    from `cargo_metadata`, and thus some of the tests had to be altered.

--- tests/multiple-bin-fail.rs
+++ tests/multiple-bin-fail.rs
@@ -1,18 +1,25 @@
 extern crate assert_cli;
 
-use assert_cli::Assert;
+use std::path::PathBuf;
 
-const EXPECTED: &str = "Error: Multiple binaries found, choose one: [src/entry1.rs, src/entry2.rs]";
+use assert_cli::Assert;
 
 #[test]
 fn multiple_bin_fail() {
     let args = ["readme", "--project-root", "tests/multiple-bin-fail"];
+
+    let base = PathBuf::from("tests/multiple-bin-fail")
+        .canonicalize()
+        .unwrap();
+    let entry1 = base.join("src/entry1.rs").to_str().unwrap().to_string();
+    let entry2 = base.join("src/entry2.rs").to_str().unwrap().to_string();
+    let expected = format!("Error: Multiple binaries found, choose one: [{entry1}, {entry2}]");
 
     Assert::main_binary()
         .with_args(&args)
         .fails()
         .and()
         .stderr()
-        .is(EXPECTED)
+        .is(&*expected)
         .unwrap();
 }
--- tests/no-entrypoint-fail.rs
+++ tests/no-entrypoint-fail.rs
@@ -1,18 +1,32 @@
 extern crate assert_cli;
 
-use assert_cli::Assert;
+use std::path::PathBuf;
 
-const EXPECTED: &str = "Error: No entrypoint found";
+use assert_cli::Assert;
 
 #[test]
 fn no_entrypoint_fail() {
     let args = ["readme", "--project-root", "tests/no-entrypoint-fail"];
+
+    let cargo_toml = PathBuf::from("tests/no-entrypoint-fail/Cargo.toml")
+        .canonicalize()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
+    let expected = format!(
+        "Error: `cargo metadata` exited with an error: error: failed to parse manifest at `{cargo_toml}`
+
+Caused by:
+  no targets specified in the manifest
+  either src/lib.rs, src/main.rs, a [lib] section, or [[bin]] section must be present"
+    );
 
     Assert::main_binary()
         .with_args(&args)
         .fails()
         .and()
         .stderr()
-        .is(EXPECTED)
+        .is(&*expected)
         .unwrap();
 }
